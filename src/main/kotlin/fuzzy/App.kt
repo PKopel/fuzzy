c0bbf1b42e80b6943f22a7b42b3d45925815ca5a
PKopel <pawel.kopel2@gmail.com>
2020-12-14 16:44:08 +0100

    improve cli, add function computing max speed, slight change in max_engine_power range

--- src/main/kotlin/fuzzy/App.kt
+++ src/main/kotlin/fuzzy/App.kt
@@ -1,3 +1,5 @@
+@file:Suppress("JoinDeclarationAndAssignment")
+
 package fuzzy
 
 import net.sourceforge.jFuzzyLogic.FIS
@@ -6,38 +8,60 @@ import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart
 import net.sourceforge.jFuzzyLogic.rule.Variable
 import kotlin.system.exitProcess
 
+//approximate values
+const val scooterWeight = 15
+const val scooterEnginePower = 300
+
+fun powerToSpeed(power: Double, weight: Double): Double =
+    power / (0.4 * (scooterWeight + weight))
+
 fun main(args: Array<String>) {
     val fileName = "src/main/resources/max_engine_power.fcl"
     val fis: FIS = FIS.load(fileName)
     val functionBlock: FunctionBlock = fis.getFunctionBlock("engine_power")
+    var input: String?
 
-    JFuzzyChart.get().chart(functionBlock)
+    print("Show charts? [y/n]:")
+    input = readLine()
+    if (input?.contains('y', true) == true)
+        JFuzzyChart.get().chart(functionBlock)
 
-    print("Enter light level (as real number between 0 and 100, default 0): ")
-    val lightLevel: Double = readLine()?.toDouble() ?: 0.0
-    print("Enter weight (as real number between 0 and 150, default 0): ")
-    val weight: Double = readLine()?.toDouble() ?: 0.0
-    print("Enter humidity (as real number between 0 and 100, default 0): ")
-    val humidity: Double = readLine()?.toDouble() ?: 0.0
-    print("Enter time of day (as real number between 0 and 23, default 0): ")
-    val timeOfDay: Double = readLine()?.toDouble() ?: 0.0
+    print("Enter light level (in %, as a real number between 0 and 100, default 80): ")
+    input = readLine()
+    val lightLevel: Double = if (input?.isNotEmpty() == true) input.toDouble() else 80.0
+    print("Enter weight (in kg, as a real number between 0 and 150, default 70): ")
+    input = readLine()
+    val weight: Double = if (input?.isNotEmpty() == true) input.toDouble() else 70.0
+    print("Enter humidity (in %, as a real number between 0 and 100, default 30): ")
+    input = readLine()
+    val humidity: Double = if (input?.isNotEmpty() == true) input.toDouble() else 30.0
+    print("Enter time of day (in hours, as a real number between 0 and 23, default 10): ")
+    input = readLine()
+    val timeOfDay: Double = if (input?.isNotEmpty() == true) input.toDouble() else 10.0
 
     fis.apply {
         setVariable(functionBlock.name, "max_engine_power", 25.0)
-        setVariable(functionBlock.name,"light", lightLevel)
-        setVariable(functionBlock.name,"time_of_day", timeOfDay)
-        setVariable(functionBlock.name,"weight", weight)
-        setVariable(functionBlock.name,"humidity", humidity)
+        setVariable(functionBlock.name, "light", lightLevel)
+        setVariable(functionBlock.name, "time_of_day", timeOfDay)
+        setVariable(functionBlock.name, "weight", weight)
+        setVariable(functionBlock.name, "humidity", humidity)
         evaluate()
     }
 
-    val volumeChange: Variable = functionBlock.getVariable("max_engine_power")
-    println("Max engine power: ${volumeChange.defuzzify()}")
-    JFuzzyChart.get().chart(volumeChange, volumeChange.defuzzifier, true)
+    val maxEnginePowerPercentage: Variable = functionBlock.getVariable("max_engine_power")
 
+    val maxEnginePower = (maxEnginePowerPercentage.defuzzify() + 30) / 100 * scooterEnginePower
+    println("Max engine power : $maxEnginePower W")
+    println("Max speed: ${powerToSpeed(maxEnginePower, weight)} m/s")
 
-    print("Press enter to close")
-    readLine()
+    print("Show chart? [y/n]:")
+    input = readLine()
+    if (input?.contains('y', true) == true) {
+        JFuzzyChart.get().chart(maxEnginePowerPercentage, maxEnginePowerPercentage.defuzzifier, true)
+        print("Press enter to close")
+        readLine()
+    }
+
     exitProcess(0)
 }
 
